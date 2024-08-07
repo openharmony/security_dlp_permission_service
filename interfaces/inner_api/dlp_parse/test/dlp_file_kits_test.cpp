9fe693235dcc5d89c07eb8c411eb7c230f5eced1
dingqiaoqiao <dingqiaoqiao@huawei.com>
2024-08-07 06:19:41 +0000

    添加用例
    Signed-off-by: dingqiaoqiao <dingqiaoqiao@huawei.com>
    Change-Id: I185bbc8a63bc0ba0b42384983ce39eaf6740d3d1

--- interfaces/inner_api/dlp_parse/test/dlp_file_kits_test.cpp
+++ interfaces/inner_api/dlp_parse/test/dlp_file_kits_test.cpp
@@ -425,3 +425,24 @@ HWTEST_F(DlpFileKitsTest, GetSandboxFlag013, TestSize.Level1)
     want.SetUri(DLP_FILE_URI_2);
     ASSERT_FALSE(DlpFileKits::GetSandboxFlag(want));
 }
+
+/**
+ * @tc.name: IsDlpFile001
+ * @tc.desc: IsDlpFile
+ * @tc.type: FUNC
+ * @tc.require:AR000H7BOC
+ */
+HWTEST_F(DlpFileKitsTest, IsDlpFile001, TestSize.Level1)
+{
+    DLP_LOG_INFO(LABEL, "IsDlpFile001");
+
+    int32_t dlpFd = -1;
+    ASSERT_FALSE(DlpFileKits::IsDlpFile(dlpFd));
+
+    dlpFd = 1;
+    ASSERT_FALSE(DlpFileKits::IsDlpFile(dlpFd));
+
+    dlpFd = open("/data/fuse_test.txt", O_CREAT | O_RDWR | O_TRUNC, 0777);
+    ASSERT_GE(dlpFd, 0);
+    ASSERT_FALSE(DlpFileKits::IsDlpFile(dlpFd));
+}
