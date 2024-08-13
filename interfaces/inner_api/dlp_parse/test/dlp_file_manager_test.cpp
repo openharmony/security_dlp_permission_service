d9f8bddf750af7d6a815633e89d1b4c9069a4134
丁巧巧 <dingqiaoqiao@huawei.com>
2024-08-13 09:20:56 +0000

    update interfaces/inner_api/dlp_parse/test/dlp_file_manager_test.cpp.
    Signed-off-by: 丁巧巧 <dingqiaoqiao@huawei.com>

--- interfaces/inner_api/dlp_parse/test/dlp_file_manager_test.cpp
+++ interfaces/inner_api/dlp_parse/test/dlp_file_manager_test.cpp
@@ -669,7 +669,7 @@ HWTEST_F(DlpFileManagerTest, RecoverDlpFile001, TestSize.Level1)
  * @tc.name: CleanTempBlob001
  * @tc.desc: test param whether is empty
  * @tc.type: FUNC
- * @tc.require:
+ * @tc.require:issue：IAIFTY
  */
 HWTEST_F(DlpFileManagerTest, CleanTempBlob001, TestSize.Level1)
 {
@@ -698,7 +698,7 @@ HWTEST_F(DlpFileManagerTest, CleanTempBlob001, TestSize.Level1)
  * @tc.name: GenerateCertBlob001
  * @tc.desc: test param whether is empty 
  * @tc.type: FUNC
- * @tc.require:
+ * @tc.require:issue：IAIFTY
  */
 HWTEST_F(DlpFileManagerTest, GenerateCertBlob001, TestSize.Level1)
 {
