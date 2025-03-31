587afd2fe4b2b119fae17cb9c98873c28486dd2e
niyisheng <niyisheng@huawei.com>
2025-03-31 15:45:12 +0800

    删除智能指针release操作
    Signed-off-by: niyisheng <niyisheng@huawei.com>

--- interfaces/kits/napi_common/include/napi_common.h
+++ interfaces/kits/napi_common/include/napi_common.h
@@ -144,11 +144,6 @@ struct OpenDlpFileSubscriberContext {
     void DeleteNapiRef(napi_env env, napi_ref ref);
 };
 
-struct OpenDlpFileUnSubscriberContext : public CommonAsyncContext {
-    explicit OpenDlpFileUnSubscriberContext(napi_env env) : CommonAsyncContext(env) {};
-    bool result = false;
-};
-
 struct GenerateDlpFileAsyncContext : public CommonAsyncContext {
     explicit GenerateDlpFileAsyncContext(napi_env env) : CommonAsyncContext(env) {};
     int64_t plaintextFd = -1;
