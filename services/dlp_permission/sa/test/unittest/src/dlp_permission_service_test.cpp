47d12483129c9972087a3867c92eddc354eb16d2
lishiwei <lishiwei32@huawei.com>
2025-08-30 07:28:21 +0000

    update services/dlp_permission/sa/test/unittest/src/dlp_permission_service_test.cpp.
    Signed-off-by: lishiwei <lishiwei32@huawei.com>

--- services/dlp_permission/sa/test/unittest/src/dlp_permission_service_test.cpp
+++ services/dlp_permission/sa/test/unittest/src/dlp_permission_service_test.cpp
@@ -1599,3 +1599,16 @@ HWTEST_F(DlpPermissionServiceTest, RemoveMDMPolicy, TestSize.Level1)
     int32_t ret = dlpPermissionService_->RemoveMDMPolicy();
     ASSERT_EQ(ret, DLP_SERVICE_ERROR_PERMISSION_DENY);
 }
+
+/**
+ * @tc.name: SetEnterprisePolicy001
+ * @tc.desc: SetEnterprisePolicy test
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DlpPermissionServiceTest, SetEnterprisePolicy001, TestSize.Level1)
+{
+    std::string policy = "policy";
+    int32_t ret = dlpPermissionService_->SetEnterprisePolicy(policy);
+    ASSERT_TRUE(ret != DLP_CALLBACK_SA_WORK_ABNORMAL);
+}
