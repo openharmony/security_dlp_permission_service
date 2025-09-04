166d1f831ca117b77637dea93c3d899a245da443
lishiwei <lishiwei32@huawei.com>
2025-09-04 06:53:27 +0000

    update services/dlp_permission/sa/mock/dlp_policy_mgr_client.h.
    Signed-off-by: lishiwei <lishiwei32@huawei.com>

--- services/dlp_permission/sa/mock/dlp_policy_mgr_client.h
+++ services/dlp_permission/sa/mock/dlp_policy_mgr_client.h
@@ -32,7 +32,7 @@ __attribute__ ((visibility("default"))) int32_t DLP_GetPolicy(PolicyType type, u
 
 __attribute__ ((visibility("default"))) int32_t DLP_CheckPermission(PolicyType type, PolicyHandle handle);
 
-__attribute__ ((visibility("default"))) int32_t DLP_SetEnterprisePolicy(uint8_t *policy, uint32_t policyLen);
+__attribute__ ((visibility("default"))) int32_t DLP_SetEnterprisePolicy(const uint8_t *policy, uint32_t policyLen);
 
 bool ModifyParseData(uint8_t** data, uint32_t* dataLen);
 #ifdef __cplusplus
