3bd4793d4394e225e36f2a9852560161b248545d
linmusheng <linmusheng1@huawei.com>
2025-10-17 14:09:38 +0800

    add api tdd test
    Signed-off-by: linmusheng <linmusheng1@huawei.com>

--- interfaces/inner_api/dlp_fuse/test/unittest/src/dlp_file_test.cpp
+++ interfaces/inner_api/dlp_fuse/test/unittest/src/dlp_file_test.cpp
@@ -303,7 +303,9 @@ HWTEST_F(DlpFileTest, TestDlpFile, TestSize.Level1)
 
     std::string fileId = "12345";
     g_Dlpfile->SetFileId(fileId);
-    ASSERT_EQ(g_Dlpfile->GetFileId(), fileId);
+    std::string fileIdRet = "";
+    g_Dlpfile->GetFileId(fileIdRet);
+    ASSERT_EQ(fileIdRet, fileId);
 
     int32_t accountType = 0;
     g_Dlpfile->SetAccountType(accountType);
