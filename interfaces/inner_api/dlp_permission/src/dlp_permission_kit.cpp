3641835d0bc8da91440336a26413e91bca8d9f5c
bishuixiu <bishuixiu1@huawei.com>
2025-11-07 16:58:02 +0800

    修改超时时间
    Signed-off-by: bishuixiu <bishuixiu1@huawei.com>

--- interfaces/inner_api/dlp_permission/src/dlp_permission_kit.cpp
+++ interfaces/inner_api/dlp_permission/src/dlp_permission_kit.cpp
@@ -28,7 +28,7 @@ namespace DlpPermission {
 namespace {
 static constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, SECURITY_DOMAIN_DLP_PERMISSION, "DlpPermissionKit"};
 const int64_t TIME_WAIT_TIME_OUT = 30;
-const int64_t PARSE_WAIT_TIME_OUT = 10;
+const int64_t PARSE_WAIT_TIME_OUT = 30;
 }  // namespace
 
 void ClientGenerateDlpCertificateCallback::OnGenerateDlpCertificate(int32_t result, const std::vector<uint8_t>& cert)
