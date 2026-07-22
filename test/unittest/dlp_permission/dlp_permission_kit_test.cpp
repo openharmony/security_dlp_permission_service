85c83eeecbf1a09e34f7e70764bb382084e562d4
ohci1 <ohci1@noreply.gitcode.com>
2026-07-22 11:38:48 +0800

    7.0
    Signed-off-by: hlzhangaj <zhanghanlin16@h-partners.com>

--- test/unittest/dlp_permission/dlp_permission_kit_test.cpp
+++ test/unittest/dlp_permission/dlp_permission_kit_test.cpp
@@ -231,7 +231,7 @@ void DlpPermissionKitTest::SetUpTestCase()
     // make test case clean
     DLP_LOG_INFO(LABEL, "SetUpTestCase.");
     g_selfTokenId = GetSelfTokenID();
-    DLP_LOG_INFO(LABEL, "get self tokenId is %{public}llu", g_selfTokenId);
+    DLP_LOG_INFO(LABEL, "get self tokenId is %{public}" PRIu64, g_selfTokenId);
     g_selfUid = getuid();
     DLP_LOG_INFO(LABEL, "get self uid is %{public}d", g_selfUid);
     DlpPermissionTestCommon::SetTestEvironment(g_selfTokenId);
