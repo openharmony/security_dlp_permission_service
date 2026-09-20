84ac7dd962db17d2fea1d01a94ca078729f01d53
wangkai479 <wangkai479@h-partners.com>
2026-09-20 11:31:57 +0800

    Signed-off-by: wangkai479 <wangkai479@h-partners.com>

--- interfaces/kits/identify_sensitive_content/napi/src/napi_identify_sensitive_content.cpp
+++ interfaces/kits/identify_sensitive_content/napi/src/napi_identify_sensitive_content.cpp
@@ -19,7 +19,6 @@
 #include "napi_dia_error_msg.h"
 #include "napi_dia_common.h"
 #include "accesstoken_kit.h"
-#include "ipc_skeleton.h"
 #include "token_setproc.h"
 
 namespace OHOS::Security::DIA {
