5206e82f888f587baaab7f745914032941a86279
wangkai479 <wangkai479@h-partners.com>
2026-09-23 09:21:39 +0800

    update: 更新文件 napi_dia_error_msg.cpp
    Signed-off-by: wangkai479 <wangkai479@h-partners.com>

--- interfaces/kits/identify_sensitive_content/napi/src/napi_dia_error_msg.cpp
+++ interfaces/kits/identify_sensitive_content/napi/src/napi_dia_error_msg.cpp
@@ -24,7 +24,7 @@ static const std::unordered_map<int32_t, std::string> DIA_JS_ERROR_MSG_MAP = {
     { ERR_DIA_JS_SUCCESS, "Success." },
     { ERR_DIA_JS_PERMISSION_DENIED,
         "Permission verification failed. The application does not have the permission required to call the API." },
-    {ERR_DIA_JS_PARAMETER_ERROR,
+    { ERR_DIA_JS_PARAMETER_ERROR,
         "Parameter error. Possible causes: 1.Mandatory parameters are left unspecified. "
         "2.Incorrect parameter types. 3.Parameter verification failed."},
     { ERR_DIA_JS_CAPABILITY_NOT_SUPPORTED,
